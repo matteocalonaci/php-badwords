4d543ce99ed91144d8b516be5f29efb2f917bc16
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-06-10 15:54:24 +0200

    creo il form

--- form.php
+++ form.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-badwords</title>
+    <title>php-badwords-form</title>
 </head>
 <body>
+    <form action="elabora.php">
+        <input type="text"placeholder="Scrivi il tuo nome" name="nome">
+        <input type="text"placeholder="Scrivi il tuo cognome" name="cognome">
+        <button type="submit">INVIA</button>
+    </form>
     
 </body>
 </html>
