c97a84cf404535ea6c360219d977880acb30e2cb
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-06-10 15:56:18 +0200

    MILESTONE 1 stampo il contenuto degli input

--- elabora.php
+++ elabora.php
@@ -1,11 +1,17 @@
+<?php
+$nome = $_GET["nome"];
+$cognome = $_GET["cognome"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-badwords-elabora</title>
 </head>
 <body>
-    <h1>Dati ricevuti?</h1>
+    <h1>Come ti chiami?</h1>
+    <h2><?php echo $nome . $cognome ?></h2>
 </body>
 </html>
