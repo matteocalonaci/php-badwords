241e3c93b9e8caadf3833d63564908678780aee1
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-06-11 09:55:07 +0200

    Add somma nome + cognome

--- elabora.php
+++ elabora.php
@@ -1,6 +1,7 @@
 <?php
 $nome = $_GET["nome"];
 $cognome = $_GET["cognome"];
+$nomeCognome = $nome . $cognome
 ?>
 
 <!DOCTYPE html>
@@ -14,5 +15,6 @@ $cognome = $_GET["cognome"];
     <h1>Come ti chiami?</h1>
     <h2><?php echo $nome . $cognome ?></h2>
     <h3>Di quante lettere è composto il tuo cognome? <?php echo strlen("$cognome"); ?></h3>
+    <h3>Di quante lettere è composto nome + cognome? <?php echo strlen("$nomeCognome"); ?></h3>
 </body>
 </html>
